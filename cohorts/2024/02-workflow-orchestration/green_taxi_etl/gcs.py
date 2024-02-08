aee2c3b1fca1f9c5a963ff8a8614c7dbd064bff4
HimaTaha <himataha88@gmail.com>
2024-02-09 00:26:03 +0200

    add schedule

--- cohorts/2024/02-workflow-orchestration/green_taxi_etl/gcs.py
+++ cohorts/2024/02-workflow-orchestration/green_taxi_etl/gcs.py
@@ -3,6 +3,7 @@ from mage_ai.io.config import ConfigFileLoader
 from mage_ai.io.google_cloud_storage import GoogleCloudStorage
 from pandas import DataFrame
 from os import path
+import os
 import pyarrow as pa
 import pyarrow.parquet as pq
 if 'data_exporter' not in globals():
@@ -17,23 +18,21 @@ def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
 
     Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
     """
-    config_path = path.join(get_repo_path(), 'io_config.yaml')
-    config_profile = 'default'
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/src/personal-gcp.json"
+    
 
     bucket_name = 'mage-zoomcamp-ibrahim'
-    object_key = 'green_taxi_data'
-
-    # table = pa.Table.from_pandas(df)
-
-    # pq.write_to_dataset(table, root_path='gs://mage-zoomcamp-ibrahim',
-    #                 partition_cols=['lpep_pickup_date'])
-
-    GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
-        df,
-        bucket_name,
-        object_key,
-        format='Parquet',
-        engine='pyarrow',
-        partition_cols=['lpep_pickup_date'],
-        
+    object_key = 'green_taxi_data.parquet'
+    root_path =f"{bucket_name}/{object_key}" 
+    partition_cols=["lpep_pickup_date"],
+    print(root_path)
+    table = pa.Table.from_pandas(df)
+    gcs = pa.fs.GcsFileSystem()
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols=partition_cols,
+        filesystem=gcs,
+        existing_data_behavior="delete_matching"
     )
+
