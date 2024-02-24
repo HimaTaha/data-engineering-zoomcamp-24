a3642bf8487cf6aebffcdf65809dc9b7ee0bb709
HimaTaha <himataha88@gmail.com>
2024-02-24 17:26:43 +0200

    restore changes from master

--- 04-analytics-engineering/taxi_rides_ny/macros/get_payment_type_description.sql
+++ 04-analytics-engineering/taxi_rides_ny/macros/get_payment_type_description.sql
@@ -1,17 +1,18 @@
-{##
-    This macro returns the description of the payment_type
+ {#
+    This macro returns the description of the payment_type 
 #}
 
-{% macro get_payment_type_description(payment_type) %}
+{% macro get_payment_type_description(payment_type) -%}
 
-        case cast( {{payment_type}} as integer )
-            when 1 then 'Credit card'
-            when 2 then 'Cash'
-            when 1 then 'No Charge'
-            when 1 then 'Dispute'
-            when 1 then 'Unkown'
-            when 1 then 'Voided trip'
-            else 'EMPTY'
-        end
+    case {{ payment_type }}
+        when 1 then 'Credit card'
+        when 2 then 'Cash'
+        when 3 then 'No charge'
+        when 4 then 'Dispute'
+        when 5 then 'Unknown'
+        when 6 then 'Voided trip'
+    end
 
 {%- endmacro %}
+
+              
